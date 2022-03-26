e9524c64d52644424e96b9c03d1eb4ec95bf8bf4
martinord <martinord@users.noreply.github.com>
2022-03-26 19:21:48 +0100

    fix: change session db index and fix bug with env var

--- models/session.ts
+++ models/session.ts
@@ -9,11 +9,11 @@ let sessionSchema: mongoose.Schema = new mongoose.Schema({
   expireAt: { 
     type: Date, 
     index: { 
-      unique: true,
+      unique: false,
       expireAfterSeconds: 0 
     },
     // Expire time set to 1h by default
-    default: (new Date()).setSeconds((new Date()).getSeconds() + ((SESSION_EXPIRE_TIME ?? 3600) as number)) },
+    default: (new Date()).setSeconds((new Date()).getSeconds() + (+(SESSION_EXPIRE_TIME ?? 3600))) },
   userId: String
 });
 
